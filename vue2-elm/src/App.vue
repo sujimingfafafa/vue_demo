67ef398fd96d6aab20c1f7b72a9dfe3418bb1cd0
sujiming <3573730266@qq.com>
2019-01-24 17:52:00 +0800

    tijiao1

---
+++ vue2-elm/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<transition name="router-fade" mode="out-in">
+			<keep-alive>
+			    <router-view></router-view>
+			</keep-alive>
+    	</transition>
+    	<!-- <transition name="router-fade" mode="out-in">
+			<router-view></router-view>
+		</transition> -->
+	
+    </div>
+</template>
+
+<script>
+
+  	export default {
+    	components:{
+
+        },
+  	}
+
+</script>
+
+<style lang="scss">
+
+	.router-fade-enter-active, .router-fade-leave-active {
+	  	transition: opacity .3s;
+	}
+	.router-fade-enter, .router-fade-leave-active {
+	  	opacity: 0;
+	}
+	*{
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		text-decoration: none;
+	}
+</style>
---
+++ vue2-elm/src/page/city/city.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+       <head-top :head-title="cityname" go-back='true'>
+            <router-link to="/home" slot="changecity" class="change_city">切换城市</router-link>
+        </head-top>
+    </div>
+</template>
+
+<script>
+import headTop from "src/components/header/head";
+  import {currentcity, searchplace} from 'src/service/getData'
+    import {getStore, setStore, removeStore} from 'src/config/mUtils'
+export default {
+  data() {
+    return {
+      inputVaule: "", // 搜索地址
+      cityid: "", // 当前城市id
+      cityname: "", // 当前城市名字
+      placelist: [], // 搜索城市列表
+      placeHistory: [], // 历史搜索记录
+      historytitle: true, // 默认显示搜索历史头部，点击搜索后隐藏
+      placeNone: false // 搜索无结果，显示提示信息
+    };
+  },
+  components:{
+      headTop
+  }
+
+
+};
+</script>
+
+<style>
+</style>
